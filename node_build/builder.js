c1e02a2b53a0c131c4d2b3ea201ef5b9274d626b
Luke Evers <github@lukevers.com>
2014-02-01 23:03:26 -0500

    Change some fs.exists to fs.open

--- node_build/builder.js
+++ node_build/builder.js
@@ -609,8 +609,6 @@ module.exports.configure = function (params, configure) {
     nThen(function(waitFor) {
         // make the build directory
         
-        console.log(params.buildDir);
-        
         Fs.open(params.buildDir, 'r', waitFor(function(err) {
             if (!err) return;
             Fs.mkdir(params.buildDir, waitFor(function (err) {
@@ -620,9 +618,22 @@ module.exports.configure = function (params, configure) {
 
     }).nThen(function(waitFor) {
 
+        Fs.open(params.buildDir, 'r', waitFor(function(err) {
+            if (err) return;
+            Fs.readFile(params.buildDir + '/state.json', waitFor(function (err, ret) {
+                if (err) { throw err; }
+                var storedState = JSON.parse(ret);
+                if (storedState.rebuildIfChangesHash === rebuildIfChangesHash) {
+                    state = storedState;
+                } else {
+                    debug("rebuildIfChanges changed, rebuilding");
+                }
+            }));
+        }));
+
         // read out the state if it exists
-        Fs.exists(params.buildDir + '/state.json', waitFor(function (exists) {
-            if (!exists) { return; }
+        Fs.open(params.buildDir + '/state.json', 'r', waitFor(function (err) {
+            if (err) { return; }
             Fs.readFile(params.buildDir + '/state.json', waitFor(function (err, ret) {
                 if (err) { throw err; }
                 var storedState = JSON.parse(ret);
